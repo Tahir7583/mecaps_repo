0d97540ba1785518108cd1c5c380d6bdc2621a53
tahir <tahirfaisal578@gmail.com>
2024-08-14 15:17:10 +0530

    new models

--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 
 
 class applicant(models.Model):
-    applicant_name=models.CharField(max_length=200)
-    city=models.CharField(max_length=200)
-    applicant_photo=models.ImageField(upload_to='profile/',blank=True,null=True)
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE,related_name='applicant')
+    gender=models.CharField(max_length=200)
+    dob=models.DateField(null=True, blank=True)
+    phone_number=models.IntegerField()
+    marital_status=models.CharField(max_length=100,null=True)
+    home_town=models.CharField(max_length=200,null=False)
+    current_location=models.CharField(max_length=200,null=False)
+    pincode=models.IntegerField()
+    curent_locations=models.CharField(max_length=100)
+    resume=models.ImageField(upload_to='resume/',blank=True,null=True)
+    preferred_job_type=models.CharField(max_length=200)
+    total_years_of_experince=models.IntegerField()
+    availability_to_join=models.CharField(max_length=200,null=True)
+    work_parmit_for_USA=models.BooleanField()
+    language=models.CharField(max_length=200)
+    about=models.CharField(max_length=400,default=True,null=True)
+
 
 
 class Skill (models.Model):
--- newproject/settings.py
+++ newproject/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MY_DATA',
-        'HOST': 'localhost',
-        'USER':'root',
-        'PASSWORD':'Tahir@123',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'HOST': 'localhost',
+        # 'USER':'root',
+        # 'PASSWORD':'Tahir@123',
+        # 'PORT':'3306'
 
     }
 }
